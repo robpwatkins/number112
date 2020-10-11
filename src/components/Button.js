2f0c14f821751f261e37768d327c04485ad990e3
robpwatkins <robpwatkins@gmail.com>
2020-10-10 21:22:52 -0500

    getting somewhere

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Button = () => {
+const Button = props => {
+    const handleClick = () => {
+        console.log(props.name)
+    }
 
+    return (
+        <div 
+            className={props.name}
+            onClick={handleClick}
+            >
+                {props.name}
+            </div>
+    )
 }
 
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Button from './Button';
 
 const Calculator = () => {
     return (
         <div className="calculator">
+            <Button name="1" />
         </div>
     )
 }
