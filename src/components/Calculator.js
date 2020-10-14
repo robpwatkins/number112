3af0a303d4302c5908d26d2983ad5ee88b3caa0c
robpwatkins <robpwatkins@gmail.com>
2020-10-14 18:57:53 -0500

    not working as i'd though so far

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -21,6 +21,7 @@ const Calculator = () => {
     const char = event.currentTarget.innerHTML;
     char === 'A/C' && clear();
     char === '±' && plusOrMinus();
+    console.log(typeof parseInt(char))
     // if (!isNaN(parseInt(char))) {
     //   setNums(nums => nums = [...nums, parseInt(char)]);
     // } else
